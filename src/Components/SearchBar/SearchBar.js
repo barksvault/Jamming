38aa4bce74e8fa53bba9bce07e061df2fe242cbb
Barkley Taylor <barkley@Barkleys-MacBook-Pro.local>
2020-01-31 01:41:41 +0100

    Inital commit

---
+++ src/Components/SearchBar/SearchBar.js
@@ -0,0 +1,12 @@
+import Reacz from "react";
+
+class SearchBar extends React.Component {
+  render() {
+    return (
+      <div className="SearchBar">
+        <input placeholder="Enter A Song, Album, or Artist" />
+        <button className="SearchButton">SEARCH</button>
+      </div>
+    );
+  }
+}
